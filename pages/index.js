c3883ce280812fba7f2e6cf86039e7d4c58063d6
Sean Groff <sgroff04@gmail.com>
2020-12-08 22:50:37 -0600

    add tailwinds and initial table styles

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Head from 'next/head'
-
 import clonewarsEpisodes from '../clonewars_episodes'
 
 function Home() {
@@ -7,26 +5,54 @@ function Home() {
     <div>
       <main>
         <h1>Star Wars: The Clone Wars Chronological Order</h1>
-        <table>
-          <tbody>
-            <td>Watched</td>
-            <td>Order</td>
-            <td>Air #</td>
-            <td>Title</td>
-            {clonewarsEpisodes.map((episode) => {
-              return (
-                <tr>
-                  <td>
-                    <input type="checkbox" value={episode.hasWatched} />
-                  </td>
-                  <td>{episode.order}</td>
-                  <td>{episode.airNumber}</td>
-                  <td>{episode.title}</td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
+        <div className="flex flex-col">
+          <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+              <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg"></div>
+              <table className="divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Title
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Air #
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Watched
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                  {clonewarsEpisodes.map((episode) => {
+                    return (
+                      <tr key={episode.title}>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                          {episode.title}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                          {episode.airNumber}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <input type="checkbox" value={episode.hasWatched} />
+                        </td>
+                      </tr>
+                    )
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   )
